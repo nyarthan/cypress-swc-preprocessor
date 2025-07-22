3a5c8906f04762ffa32a21913b2896390dd9962d
Jannis Morgenstern <jannis.n.morgenstern@gmail.com>
2025-07-22 18:18:51 +0200

    feat: swc transformer

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,40 @@
-console.log("Hello, World!");
+import type { EventEmitter } from "node:events";
+
+import type { Options as SWCOptions } from "@swc/core";
+
+import { transformFile } from "@swc/core";
+
+type CypressFile = {
+  filePath: string;
+  outputPath: string;
+  shouldWatch: boolean;
+} & EventEmitter;
+
+type Preprocessor = (file: CypressFile) => Promise<string>;
+
+type Cache = Map<string, string>;
+
+export function swcPreprocessor(): Preprocessor {
+  const cache: Cache = new Map();
+
+  return getPreprocessor(cache);
+}
+
+function getPreprocessor(cache: Cache): Preprocessor {
+  return async function preprocess(file) {
+    if (!file.shouldWatch) {
+      await build(file.filePath, { outputPath: file.outputPath });
+      return file.outputPath;
+    }
+
+    await build(file.filePath, { outputPath: file.outputPath });
+    return file.outputPath;
+  };
+}
+
+async function build(
+  filePath: string,
+  swcOptions: SWCOptions & { outputPath: string },
+): Promise<void> {
+  await transformFile(filePath, swcOptions);
+}
