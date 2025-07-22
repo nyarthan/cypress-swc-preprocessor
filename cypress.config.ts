d67848c492b31b63e8d7a57d25591d266fa0708c
Jannis Morgenstern <jannis.n.morgenstern@gmail.com>
2025-07-22 19:03:19 +0200

    feat: file watcher

--- cypress.config.ts
+++ cypress.config.ts
@@ -1,7 +1,13 @@
 import { defineConfig } from "cypress";
 
+import { swcPreprocessor } from "./src";
+
 export default defineConfig({
   e2e: {
+    watchForFileChanges: true,
     supportFile: false,
+    setupNodeEvents(on) {
+      on("file:preprocessor", swcPreprocessor());
+    },
   },
 });
--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,12 @@
 import type { EventEmitter } from "node:events";
 
 import type { Options as SWCOptions } from "@swc/core";
+import type { FSWatcher } from "chokidar";
+
+import { writeFile } from "node:fs/promises";
 
 import { transformFile } from "@swc/core";
+import { watch } from "chokidar";
 
 type CypressFile = {
   filePath: string;
@@ -13,21 +17,49 @@ type CypressFile = {
 type Preprocessor = (file: CypressFile) => Promise<string>;
 
 type Cache = Map<string, string>;
+type Watchers = Map<string, FSWatcher>;
 
 export function swcPreprocessor(): Preprocessor {
   const cache: Cache = new Map();
+  const watchers = new Map();
 
-  return getPreprocessor(cache);
+  return getPreprocessor(cache, watchers);
 }
 
-function getPreprocessor(cache: Cache): Preprocessor {
+function getPreprocessor(cache: Cache, watchers: Watchers): Preprocessor {
   return async function preprocess(file) {
+    const cachedOutputPath = cache.get(file.filePath);
+    if (cachedOutputPath) return cachedOutputPath;
+
+    await build(file.filePath, { outputPath: file.outputPath });
+    cache.set(file.filePath, file.outputPath);
+
     if (!file.shouldWatch) {
-      await build(file.filePath, { outputPath: file.outputPath });
       return file.outputPath;
     }
 
-    await build(file.filePath, { outputPath: file.outputPath });
+    if (watchers.has(file.filePath)) {
+      return file.outputPath;
+    }
+
+    const watcher = watch(file.filePath, { ignoreInitial: true });
+
+    watcher.on("change", async () => {
+      await build(file.filePath, { outputPath: file.outputPath });
+      file.emit("rerun");
+    });
+
+    watchers.set(file.filePath, watcher);
+
+    file.on("close", () => {
+      const watcher = watchers.get(file.filePath);
+      if (watcher) {
+        watcher.close();
+        watchers.delete(file.filePath);
+      }
+      cache.delete(file.filePath);
+    });
+
     return file.outputPath;
   };
 }
@@ -36,5 +68,7 @@ async function build(
   filePath: string,
   swcOptions: SWCOptions & { outputPath: string },
 ): Promise<void> {
-  await transformFile(filePath, swcOptions);
+  await transformFile(filePath, swcOptions).then((output) =>
+    writeFile(swcOptions.outputPath, output.code),
+  );
 }
